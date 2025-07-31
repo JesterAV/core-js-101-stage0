691644447dda817d89393232cb948b3ef124804a
JesterAV <winchesrers@gmail.com>
2025-07-31 21:17:03 +0300

    feat: implement isPrime function

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -205,7 +205,7 @@ function roundToPowerOfTen(num, pow) {
  * Returns true is the number is prime; otherwise false.
  * See: https://en.wikipedia.org/wiki/Primality_test
  *
- * @param {number} n
+ * @param {number}
  * @return {bool}
  *
  * @example:
@@ -218,8 +218,13 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
 }
 
 /**
