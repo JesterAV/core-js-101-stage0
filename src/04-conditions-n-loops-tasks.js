f444a147d7fdc948d981788d7c54f220dd38f08b
JesterAV <winchesrers@gmail.com>
2025-08-05 14:59:50 +0300

    feat: implement isTriangle, doRectanglesOverlap and isInsideCircle functions

--- src/04-conditions-n-loops-tasks.js
+++ src/04-conditions-n-loops-tasks.js
@@ -98,8 +98,12 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle(a, b, c) {
+  if (a > 1 && b > 1 && c > 1) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -134,8 +138,20 @@ function isTriangle(/* a, b, c */) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  const left1 = rect1.left;
+  const right1 = rect1.left + rect1.width;
+  const top1 = rect1.top;
+  const bottom1 = rect1.top + rect1.height;
+
+  const left2 = rect2.left;
+  const right2 = rect2.left + rect2.width;
+  const top2 = rect2.top;
+  const bottom2 = rect2.top + rect2.height;
+
+  if (right1 <= left2 || right2 <= left1 || bottom1 <= top2 || bottom2 <= top1)
+    return false;
+  return true;
 }
 
 /**
@@ -164,8 +180,14 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const { center, radius } = circle;
+  const dx = point.x - center.x;
+  const dy = point.y - center.y;
+  const distanceSquared = dx * dx + dy * dy;
+  const radiusSquared = radius * radius;
+
+  return distanceSquared < radiusSquared;
 }
 
 /**
